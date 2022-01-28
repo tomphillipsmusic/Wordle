0b6331c499b65b40a087b3327cda1568c54be0f5
Tom Phillips <tomphillipsmusic@gmail.com>
2022-01-28 18:49:56 -0500

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Tom Phillips on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
